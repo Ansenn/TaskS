4691c72d6f2e27970dda8cc746d59f375cf89992
Ansenn <ansenn.lee@gmail.com>
2023-03-13 23:36:31 +0800

    Add methods to print deadline and print data using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,7 +21,10 @@ public class Main {
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines counted using streams: " + countDeadlinesUsingStream(tasksData));
+        // printData(tasksData);
+        // printDataUsingStreams(tasksData);
+
 
     }
 
@@ -44,16 +47,31 @@ public class Main {
     }
 
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing Data using iteration : ");
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    public static void printDataUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using streams: ");
+        tasks.stream()
+                .forEach(System.out::println);
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using iteration: ");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing deadlines using streams: ");
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .forEach(System.out::println);
     }
 }
